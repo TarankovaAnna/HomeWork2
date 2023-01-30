3968ef773aa92530c7794de8f2b48a6cb571b49a
Anna Tarankova <tarankovaanna@gmail.com>
2023-01-30 23:36:27 +0300

    Change2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         int ticket = 15622;
-        int mile = ticket/20;
+        int mile = ticket / 20;
 
-        System.out.println ("На Вашем счете:" + mile + "миль");
+        System.out.println("На Вашем счете:" + mile + "миль");
     }
 }

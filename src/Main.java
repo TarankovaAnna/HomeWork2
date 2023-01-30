d0acd429753270d9aeb906f571b7171d80b75179
Anna Tarankova <tarankovaanna@gmail.com>
2023-01-30 19:14:16 +0300

    Add src

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 15622;
+        int mile = ticket/20;
+
+        System.out.println ("На Вашем счете:" + mile + "миль");
+    }
+}
